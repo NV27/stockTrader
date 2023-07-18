3e6a391c1eb39ea87ffe6f93ddb1b70c0a444799
NV27 <louisprotano@hotmail.com>
2023-07-18 19:30:17 +0100

    2nd commit

--- stockTrader.py
+++ stockTrader.py
@@ -8,16 +8,17 @@ import random
 class Stock:
     def __init__(self, name, startingPrice):
         self.name = name
-        self.startingPrice = 0
+        self.startingPrice = startingPrice
 
-def getStock():
-    print(str(Stock.name) + "Price is: " + Stock.startingPrice)
+def getStock(stock):
+    print(stock.name + " price is: " + str(stock.startingPrice))
 
 # Define objects
+
 turnip = Stock("turnip", 5)
 potato = Stock("potato", 3)
 
 
 
 #Game
-getStock()
+getStock(potato)
